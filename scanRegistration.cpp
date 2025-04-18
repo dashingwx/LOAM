c4b78a168fe8a7dc60aeda5b95cbff0c68c13334
dashingwx <148050320+dashingwx@users.noreply.github.com>
2025-04-18 17:38:24 +0800

    Create scanRegistration.cpp

---
+++ scanRegistration.cpp
@@ -0,0 +1,275 @@
+// Copyright 2013, Ji Zhang, Carnegie Mellon University
+// Further contributions copyright (c) 2016, Southwest Research Institute
+// All rights reserved.
+//
+// Redistribution and use in source and binary forms, with or without
+// modification, are permitted provided that the following conditions are met:
+//
+// 1. Redistributions of source code must retain the above copyright notice,
+//    this list of conditions and the following disclaimer.
+// 2. Redistributions in binary form must reproduce the above copyright notice,
+//    this list of conditions and the following disclaimer in the documentation
+//    and/or other materials provided with the distribution.
+// 3. Neither the name of the copyright holder nor the names of its
+//    contributors may be used to endorse or promote products derived from this
+//    software without specific prior written permission.
+//
+// THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+// AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+// IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+// ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+// LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+// CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+// SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+// INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+// CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+// ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+// POSSIBILITY OF SUCH DAMAGE.
+//
+// This is an implementation of the algorithm described in the following paper:
+//   J. Zhang and S. Singh. LOAM: Lidar Odometry and Mapping in Real-time.
+//     Robotics: Science and Systems Conference (RSS). Berkeley, CA, July 2014.
+
+
+
+/******************************读前须知*****************************************/
+/*imu为x轴向前,y轴向左,z轴向上的右手坐标系，
+  velodyne lidar被安装为x轴向前,y轴向左,z轴向上的右手坐标系，
+  scanRegistration会把两者通过交换坐标轴，都统一到z轴向前,x轴向左,y轴向上的右手坐标系
+  ，这是J. Zhang的论文里面使用的坐标系
+  交换后：R = Ry(yaw)*Rx(pitch)*Rz(roll)
+*******************************************************************************/
+
+#include <cmath>
+#include <vector>
+
+#include <loam_velodyne/common.h>
+#include <opencv/cv.h>
+#include <nav_msgs/Odometry.h>
+#include <opencv/cv.h>
+#include <pcl_conversions/pcl_conversions.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <ros/ros.h>
+#include <sensor_msgs/Imu.h>
+#include <sensor_msgs/PointCloud2.h>
+#include <tf/transform_datatypes.h>
+#include <tf/transform_broadcaster.h>
+
+using std::sin;
+using std::cos;
+using std::atan2;
+
+//扫描周期, velodyne频率10Hz，周期0.1s
+const double scanPeriod = 0.1;
+
+//初始化控制变量
+const int systemDelay = 20;//弃用前20帧初始数据
+int systemInitCount = 0;
+bool systemInited = false;
+
+//激光雷达线数
+const int N_SCANS = 16;
+
+//点云曲率, 40000为一帧点云中点的最大数量
+float cloudCurvature[40000];
+//曲率点对应的序号
+int cloudSortInd[40000];
+//点是否筛选过标志：0-未筛选过，1-筛选过
+int cloudNeighborPicked[40000];
+//点分类标号:2-代表曲率很大，1-代表曲率比较大,-1-代表曲率很小，0-曲率比较小(其中1包含了2,0包含了1,0和1构成了点云全部的点)
+int cloudLabel[40000];
+
+//imu时间戳大于当前点云时间戳的位置
+int imuPointerFront = 0;
+//imu最新收到的点在数组中的位置
+int imuPointerLast = -1;
+//imu循环队列长度
+const int imuQueLength = 200;
+
+//点云数据开始第一个点的位移/速度/欧拉角
+float imuRollStart = 0, imuPitchStart = 0, imuYawStart = 0;
+float imuRollCur = 0, imuPitchCur = 0, imuYawCur = 0;
+
+float imuVeloXStart = 0, imuVeloYStart = 0, imuVeloZStart = 0;
+float imuShiftXStart = 0, imuShiftYStart = 0, imuShiftZStart = 0;
+
+//当前点的速度，位移信息
+float imuVeloXCur = 0, imuVeloYCur = 0, imuVeloZCur = 0;
+float imuShiftXCur = 0, imuShiftYCur = 0, imuShiftZCur = 0;
+
+//每次点云数据当前点相对于开始第一个点的畸变位移，速度
+float imuShiftFromStartXCur = 0, imuShiftFromStartYCur = 0, imuShiftFromStartZCur = 0;
+float imuVeloFromStartXCur = 0, imuVeloFromStartYCur = 0, imuVeloFromStartZCur = 0;
+
+//IMU信息
+double imuTime[imuQueLength] = {0};
+float imuRoll[imuQueLength] = {0};
+float imuPitch[imuQueLength] = {0};
+float imuYaw[imuQueLength] = {0};
+
+float imuAccX[imuQueLength] = {0};
+float imuAccY[imuQueLength] = {0};
+float imuAccZ[imuQueLength] = {0};
+
+float imuVeloX[imuQueLength] = {0};
+float imuVeloY[imuQueLength] = {0};
+float imuVeloZ[imuQueLength] = {0};
+
+float imuShiftX[imuQueLength] = {0};
+float imuShiftY[imuQueLength] = {0};
+float imuShiftZ[imuQueLength] = {0};
+//代码末尾的ros::Publisher定义了多个ROS话题发布器
+ros::Publisher pubLaserCloud;         // 发布去畸变后的完整点云（/velodyne_cloud_2）
+ros::Publisher pubCornerPointsSharp;   // 发布边缘特征点（/laser_cloud_sharp）
+ros::Publisher pubCornerPointsLessSharp;  // 发布次边缘特征点（/laser_cloud_less_sharp）
+ros::Publisher pubSurfPointsFlat;     // 发布平面特征点（/laser_cloud_flat）
+ros::Publisher pubSurfPointsLessFlat; // 发布次平面特征点（/laser_cloud_less_flat）
+ros::Publisher pubImuTrans;           // 发布IMU坐标系到世界坐标系的变换（/imu_trans）
+
+//计算局部坐标系下点云中的点相对第一个开始点的由于加减速运动产生的位移畸变
+void ShiftToStartIMU(float pointTime)
+{
+  //计算相对于第一个点由于加减速产生的畸变位移(全局坐标系下畸变位移量delta_Tg)
+  //imuShiftFromStartCur = imuShiftCur - (imuShiftStart + imuVeloStart * pointTime)整体公式的目的是从当前位移（imuShiftXCur）中扣除初始位移（imuShiftXStart）和匀速运动分量，得到纯由加减速引起的畸变位移
+  imuShiftFromStartXCur = imuShiftXCur - imuShiftXStart - imuVeloXStart * pointTime;
+  imuShiftFromStartYCur = imuShiftYCur - imuShiftYStart - imuVeloYStart * pointTime;
+  imuShiftFromStartZCur = imuShiftZCur - imuShiftZStart - imuVeloZStart * pointTime;
+
+  /********************************************************************************
+  Rz(pitch).inverse * Rx(pitch).inverse * Ry(yaw).inverse * delta_Tg
+  transfrom from the global frame to the local frame
+  *********************************************************************************/
+
+  //绕y轴旋转(-imuYawStart)，即Ry(yaw).inverse
+  float x1 = cos(imuYawStart) * imuShiftFromStartXCur - sin(imuYawStart) * imuShiftFromStartZCur;
+  float y1 = imuShiftFromStartYCur;
+  float z1 = sin(imuYawStart) * imuShiftFromStartXCur + cos(imuYawStart) * imuShiftFromStartZCur;
+
+  //绕x轴旋转(-imuPitchStart)，即Rx(pitch).inverse
+  float x2 = x1;
+  float y2 = cos(imuPitchStart) * y1 + sin(imuPitchStart) * z1;
+  float z2 = -sin(imuPitchStart) * y1 + cos(imuPitchStart) * z1;
+
+  //绕z轴旋转(-imuRollStart)，即Rz(pitch).inverse
+  imuShiftFromStartXCur = cos(imuRollStart) * x2 + sin(imuRollStart) * y2;
+  imuShiftFromStartYCur = -sin(imuRollStart) * x2 + cos(imuRollStart) * y2;
+  imuShiftFromStartZCur = z2;
+}
+
+//计算局部坐标系下点云中的点相对第一个开始点由于加减速产生的的速度畸变（增量）
+void VeloToStartIMU()
+{
+  //计算相对于第一个点由于加减速产生的畸变速度(全局坐标系下畸变速度增量delta_Vg)
+  imuVeloFromStartXCur = imuVeloXCur - imuVeloXStart;
+  imuVeloFromStartYCur = imuVeloYCur - imuVeloYStart;
+  imuVeloFromStartZCur = imuVeloZCur - imuVeloZStart;
+
+  /********************************************************************************
+    Rz(pitch).inverse * Rx(pitch).inverse * Ry(yaw).inverse * delta_Vg
+    transfrom from the global frame to the local frame
+  *********************************************************************************/
+  
+  //绕y轴旋转(-imuYawStart)，即Ry(yaw).inverse
+  float x1 = cos(imuYawStart) * imuVeloFromStartXCur - sin(imuYawStart) * imuVeloFromStartZCur;
+  float y1 = imuVeloFromStartYCur;
+  float z1 = sin(imuYawStart) * imuVeloFromStartXCur + cos(imuYawStart) * imuVeloFromStartZCur;
+
+  //绕x轴旋转(-imuPitchStart)，即Rx(pitch).inverse
+  float x2 = x1;
+  float y2 = cos(imuPitchStart) * y1 + sin(imuPitchStart) * z1;
+  float z2 = -sin(imuPitchStart) * y1 + cos(imuPitchStart) * z1;
+
+  //绕z轴旋转(-imuRollStart)，即Rz(pitch).inverse
+  imuVeloFromStartXCur = cos(imuRollStart) * x2 + sin(imuRollStart) * y2;
+  imuVeloFromStartYCur = -sin(imuRollStart) * x2 + cos(imuRollStart) * y2;
+  imuVeloFromStartZCur = z2;
+}
+
+//去除点云加减速产生的位移畸变
+void TransformToStartIMU(PointType *p)
+{
+  /********************************************************************************
+    Ry*Rx*Rz*Pl, transform point to the global frame
+  *********************************************************************************///反映扫描过程中的动态姿态变化
+  //绕z轴旋转(imuRollCur)
+  float x1 = cos(imuRollCur) * p->x - sin(imuRollCur) * p->y;
+  float y1 = sin(imuRollCur) * p->x + cos(imuRollCur) * p->y;
+  float z1 = p->z;
+
+  //绕x轴旋转(imuPitchCur)
+  float x2 = x1;
+  float y2 = cos(imuPitchCur) * y1 - sin(imuPitchCur) * z1;
+  float z2 = sin(imuPitchCur) * y1 + cos(imuPitchCur) * z1;
+
+  //绕y轴旋转(imuYawCur)
+  float x3 = cos(imuYawCur) * x2 + sin(imuYawCur) * z2;
+  float y3 = y2;
+  float z3 = -sin(imuYawCur) * x2 + cos(imuYawCur) * z2;
+
+  /********************************************************************************
+    Rz(pitch).inverse * Rx(pitch).inverse * Ry(yaw).inverse * Pg
+    transfrom global points to the local frame
+  *********************************************************************************///目的是消除初始姿态对点云的影响
+  
+  //绕y轴旋转(-imuYawStart)
+  float x4 = cos(imuYawStart) * x3 - sin(imuYawStart) * z3;
+  float y4 = y3;
+  float z4 = sin(imuYawStart) * x3 + cos(imuYawStart) * z3;
+
+  //绕x轴旋转(-imuPitchStart)
+  float x5 = x4;
+  float y5 = cos(imuPitchStart) * y4 + sin(imuPitchStart) * z4;
+  float z5 = -sin(imuPitchStart) * y4 + cos(imuPitchStart) * z4;
+
+  //绕z轴旋转(-imuRollStart)，然后叠加平移量
+  p->x = cos(imuRollStart) * x5 + sin(imuRollStart) * y5 + imuShiftFromStartXCur;
+  p->y = -sin(imuRollStart) * x5 + cos(imuRollStart) * y5 + imuShiftFromStartYCur;
+  p->z = z5 + imuShiftFromStartZCur;
+}
+
+//积分速度与位移,先积分得到imuPointLast的值
+void AccumulateIMUShift()
+{
+  float roll = imuRoll[imuPointerLast];
+  float pitch = imuPitch[imuPointerLast];
+  float yaw = imuYaw[imuPointerLast];
+  float accX = imuAccX[imuPointerLast];
+  float accY = imuAccY[imuPointerLast];
+  float accZ = imuAccZ[imuPointerLast];
+
+  //将当前时刻的加速度值绕交换过的ZXY固定轴（原XYZ）分别旋转(roll, pitch, yaw)角，转换得到世界坐标系下的加速度值(right hand rule)
+  //绕z轴旋转(roll)
+  float x1 = cos(roll) * accX - sin(roll) * accY;
+  float y1 = sin(roll) * accX + cos(roll) * accY;
+  float z1 = accZ;
+  //绕x轴旋转(pitch)
+  float x2 = x1;
+  float y2 = cos(pitch) * y1 - sin(pitch) * z1;
+  float z2 = sin(pitch) * y1 + cos(pitch) * z1;
+  //绕y轴旋转(yaw)
+  accX = cos(yaw) * x2 + sin(yaw) * z2;
+  accY = y2;
+  accZ = -sin(yaw) * x2 + cos(yaw) * z2;
+
+  //上一个imu点
+  int imuPointerBack = (imuPointerLast + imuQueLength - 1) % imuQueLength;
+  //上一个点到当前点所经历的时间，即计算imu测量周期
+  double timeDiff = imuTime[imuPointerLast] - imuTime[imuPointerBack];
+  //要求imu的频率至少比lidar高，这样的imu信息才使用，后面校正也才有意义
+  if (timeDiff < scanPeriod) {//（隐含从静止开始运动）
+    //求每个imu时间点的位移与速度,两点之间视为匀加速直线运动
+    imuShiftX[imuPointerLast] = imuShiftX[imuPointerBack] + imuVeloX[imuPointerBack] * timeDiff 
+                              + accX * timeDiff * timeDiff / 2;
+    imuShiftY[imuPointerLast] = imuShiftY[imuPointerBack] + imuVeloY[imuPointerBack] * timeDiff 
+                              + accY * timeDiff * timeDiff / 2;
+    imuShiftZ[imuPointerLast] = imuShiftZ[imuPointerBack] + imuVeloZ[imuPointerBack] * timeDiff 
+                              + accZ * timeDiff * timeDiff / 2;
+
+    imuVeloX[imuPointerLast] = imuVeloX[imuPointerBack] + accX * timeDiff;
+    imuVeloY[imuPointerLast] = imuVeloY[imuPointerBack] + accY * timeDiff;
+    imuVeloZ[imuPointerLast] = imuVeloZ[imuPointerBack] + accZ * timeDiff;
+  }
+}
